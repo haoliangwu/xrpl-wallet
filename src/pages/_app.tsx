fab98454ee86078092cd5f96baf91d0c4e50a1d4
haoliang.wu <haoliang.wu@sbibits.com>
2023-04-27 12:23:20 +0800

    basic config

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,12 @@
-import '~/styles/globals.css'
-import type { AppProps } from 'next/app'
+import "@unocss/reset/tailwind.css";
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+import "../styles/globals.css"
+import "../styles/uno.css";
+
+import type { AppProps } from "next/app";
+
+function MyApp({ Component, pageProps }: AppProps) {
+  return <Component {...pageProps} />;
 }
+
+export default MyApp;
