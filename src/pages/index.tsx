5663767f1045d632a2e5540cd6b08290d3d381c2
haoliangwu <littlelyon@aliyun.com>
2023-04-28 19:30:42 +0800

    fix: the floating layout of dashboard

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -100,99 +100,93 @@ export default function Home() {
   }, [client, wallets, syncAccountInfo, syncAccountTxHistory]);
 
   return wallet.isSome() ? (
-    <div className="w-1000px mx-auto">
-      <div className="bg-#ffffff p-4">
-        <div className="h-64px text-center">
-          <Typography.Title level={4}>Address</Typography.Title>
-          <Typography.Text copyable>
-            {wallet.cata(
-              () => "-",
-              (w) => w.address
-            )}
-          </Typography.Text>
+    <>
+      <div className="h-64px text-center">
+        <Typography.Title level={4}>Address</Typography.Title>
+        <Typography.Text copyable>
+          {wallet.cata(
+            () => "-",
+            (w) => w.address
+          )}
+        </Typography.Text>
+      </div>
+      <Divider />
+      <div className="flex flex-col items-center">
+        <Typography.Title level={4}>Balance</Typography.Title>
+        <div>
+          {account?.Balance ? dropsToXrp(account.Balance) : "-"}
+          <span className="ml-1">XRP</span>
         </div>
-        <Divider />
-        <div className="flex flex-col items-center">
-          <Typography.Title level={4}>Balance</Typography.Title>
-          <div>
-            {account?.Balance ? dropsToXrp(account.Balance) : "-"}
-            <span className="ml-1">XRP</span>
-          </div>
-          <div className="flex gap-4 mt-4">
-            <Button
-              type="primary"
-              onClick={() => {
-                router.push("/send-tx");
-              }}
-            >
-              Send Tx
-            </Button>
-            <Button
-              type="primary"
-              onClick={() => {
-                router.push("/nft");
-              }}
-            >
-              NFT
-            </Button>
-          </div>
+        <div className="flex gap-4 mt-4">
+          <Button
+            type="primary"
+            onClick={() => {
+              router.push("/send-tx");
+            }}
+          >
+            Send Tx
+          </Button>
+          <Button
+            type="primary"
+            onClick={() => {
+              router.push("/nft");
+            }}
+          >
+            NFT
+          </Button>
         </div>
-        <Divider />
-        <div className="flex flex-col items-center">
-          <Typography.Title level={4}>History</Typography.Title>
-          <div className="w-full max-h-500px overflow-y-auto ">
-            {txHistory.map(({ tx }) => {
-              const isRecipient = wallet.every(
-                (w) => w.address === (tx as Payment).Destination
-              );
+      </div>
+      <Divider />
+      <div className="flex flex-col items-center">
+        <Typography.Title level={4}>History</Typography.Title>
+        <div className="w-full max-h-500px overflow-y-auto ">
+          {txHistory.map(({ tx }) => {
+            const isRecipient = wallet.every(
+              (w) => w.address === (tx as Payment).Destination
+            );
 
-              return (
-                <div
-                  className="px-4 py-2 border-b-1 border-b-#d6d9dc border-b-solid"
-                  key={tx?.hash}
-                >
-                  <div className="flex">
-                    <span className="mr-2">{tx?.TransactionType}</span>
-                    <span className="flex-auto"></span>
-                    {tx?.TransactionType === "Payment" && (
-                      <Typography.Text>
-                        {dropsToXrp(tx.Amount.toString())} XRP (
-                        {isRecipient ? "Received" : "Sent"})
-                      </Typography.Text>
-                    )}
-                    {tx?.TransactionType === "NFTokenMint" && (
-                      <Typography.Text copyable>
-                        {hexDecode(tx.URI ?? "-")}
-                      </Typography.Text>
-                    )}
-                  </div>
-                  <div className="flex">
-                    <span className="mr-2">Tx Hash</span>
-                    <span className="flex-auto"></span>
-                    <Typography.Text copyable={{ text: tx?.hash }}>
-                      {tx?.hash}
+            return (
+              <div
+                className="px-4 py-2 border-b-1 border-b-#d6d9dc border-b-solid"
+                key={tx?.hash}
+              >
+                <div className="flex">
+                  <span className="mr-2">{tx?.TransactionType}</span>
+                  <span className="flex-auto"></span>
+                  {tx?.TransactionType === "Payment" && (
+                    <Typography.Text>
+                      {dropsToXrp(tx.Amount.toString())} XRP (
+                      {isRecipient ? "Received" : "Sent"})
                     </Typography.Text>
-                  </div>
+                  )}
+                  {tx?.TransactionType === "NFTokenMint" && (
+                    <Typography.Text copyable>
+                      {hexDecode(tx.URI ?? "-")}
+                    </Typography.Text>
+                  )}
                 </div>
-              );
-            })}
-          </div>
+                <div className="flex">
+                  <span className="mr-2">Tx Hash</span>
+                  <span className="flex-auto"></span>
+                  <Typography.Text copyable={{ text: tx?.hash }}>
+                    {tx?.hash}
+                  </Typography.Text>
+                </div>
+              </div>
+            );
+          })}
         </div>
       </div>
-    </div>
+    </>
   ) : (
-    <div className="relative w-100vw h-100vh">
-      <div className="absolute top-50% left-50% transform translate-x-[-50%] translate-y-[-50%]">
-        <Result
-          icon={<SmileOutlined />}
-          title="Oops, it seems you don't have account yet!"
-          extra={
-            <Button type="primary" onClick={() => router.push("/create")}>
-              Create
-            </Button>
-          }
-        />
-      </div>
-    </div>
+    <Result
+      icon={<SmileOutlined />}
+      title="Oops, it seems you don't have account yet!"
+      extra={
+        <Button type="primary" onClick={() => router.push("/create")}>
+          Create
+        </Button>
+      }
+    />
   );
 }
