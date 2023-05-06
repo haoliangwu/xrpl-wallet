f4dcdc2254a1e2a1f0171f0e95eba28be37b23c2
haoliang.wu <haoliang.wu@sbibits.com>
2023-05-06 12:00:47 +0800

    imp: infinite load in dashboard

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,13 @@
 import { useCallback, useEffect, useState } from "react";
 import { SmileOutlined } from "@ant-design/icons";
-import { Typography, Divider, Button, Result, Descriptions } from "antd";
+import {
+  Typography,
+  Divider,
+  Button,
+  Result,
+  Descriptions,
+  Skeleton,
+} from "antd";
 import {
   AccountInfoResponse,
   dropsToXrp,
@@ -12,6 +19,7 @@ import {
 } from "xrpl";
 import { useRouter } from "next/router";
 import { Maybe } from "monet";
+import InfiniteScroll from "react-infinite-scroll-component";
 
 import {
   useXrpLedgerClient,
@@ -22,6 +30,7 @@ import { parseAccountId } from "~/utils";
 
 export default function Home() {
   const router = useRouter();
+  const [loading, setLoading] = useState(false);
   const [account, setAccount] = useState<
     Maybe<AccountInfoResponse["result"]["account_data"]>
   >(Maybe.None());
@@ -29,11 +38,11 @@ export default function Home() {
     AccountNFTsResponse["result"]["account_nfts"]
   >([]);
 
-  const { client, network, setNetwork } = useXrpLedgerClient();
-  const { wallet, wallets, setWallet } = useXrpLedgerWallet();
+  const { client } = useXrpLedgerClient();
+  const { wallet, wallets } = useXrpLedgerWallet();
   const [txHistory, setTxHistory] = useState<
-    AccountTxResponse["result"]["transactions"]
-  >([]);
+    Maybe<AccountTxResponse["result"]>
+  >(Maybe.None());
 
   const syncAccountInfo = useCallback(() => {
     wallet
@@ -78,15 +87,58 @@ export default function Home() {
           c.request({
             command: "account_tx",
             account: w.address,
+            limit: 10,
           })
       )
       .apTo(client)
       .forEach((defer) => {
         defer.then((res) => {
-          setTxHistory(res.result.transactions);
+          setTxHistory(Maybe.Some(res.result));
         });
       });
   }, [client, wallet]);
+
+  const loadMoreTxHistory = () => {
+    if (loading) {
+      return;
+    }
+
+    wallet
+      .map(
+        (w) => (c: Client) => (previousPage: AccountTxResponse["result"]) => {
+          setLoading(true);
+          return c
+            .request({
+              command: "account_tx",
+              account: w.address,
+              limit: 20,
+              marker: previousPage.marker,
+            })
+            .then((res) => {
+              return {
+                ...res,
+                result: {
+                  ...res.result,
+                  transactions: [
+                    ...previousPage.transactions,
+                    ...res.result.transactions,
+                  ],
+                },
+              };
+            })
+            .finally(() => {
+              setLoading(false);
+            });
+        }
+      )
+      .apTo(client)
+      .apTo(txHistory)
+      .forEach((defer) => {
+        defer.then((res) => {
+          setTxHistory(Maybe.Some(res.result));
+        });
+      });
+  };
 
   // init logic when comp is mounted
   useEffect(() => {
@@ -157,64 +209,78 @@ export default function Home() {
       <Divider />
       <div className="flex flex-col items-center">
         <Typography.Title level={3}>TX History</Typography.Title>
-        <div className="w-full max-h-500px overflow-y-auto ">
-          {txHistory.map(({ tx }) => {
-            const isRecipient = wallet.every(
-              (w) => w.address === (tx as Payment).Destination
-            );
+        <div id="scrollableDiv" className="w-full max-h-500px overflow-y-auto">
+          <InfiniteScroll
+            dataLength={txHistory.map((h) => h.transactions.length).orSome(0)}
+            next={loadMoreTxHistory}
+            hasMore={txHistory.every((h) => Boolean(h.marker))}
+            loader={
+              <Skeleton className="mt-6" paragraph={{ rows: 1 }} active />
+            }
+            endMessage={<Divider plain>It is all, nothing more 🤐</Divider>}
+            scrollableTarget="scrollableDiv"
+          >
+            {txHistory
+              .map((h) => h.transactions.map((t) => t.tx))
+              .orSome([])
+              .map((tx) => {
+                const isRecipient = wallet.every(
+                  (w) => w.address === (tx as Payment).Destination
+                );
 
-            return (
-              <div
-                className="px-4 py-2 border-b-1 border-b-#d6d9dc border-b-solid mb-1"
-                key={tx?.hash}
-              >
-                <div className="flex mb-2">
-                  <span className="mr-2">Sequence</span>
-                  <span className="flex-auto"></span>
-                  <Typography.Text className="text-xs">
-                    {tx?.Sequence === 0
-                      ? `${tx.TicketSequence} (Ticket)`
-                      : tx?.Sequence}
-                  </Typography.Text>
-                </div>
-                <div className="flex">
-                  <span className="mr-2">Tx Hash</span>
-                  <span className="flex-auto"></span>
-                  <ScannerText
-                    className="text-xs"
-                    href={`/transactions/${tx?.hash}`}
+                return (
+                  <div
+                    className="px-4 py-2 border-b-1 border-b-#d6d9dc border-b-solid mb-1"
+                    key={tx?.hash}
                   >
-                    {tx?.hash}
-                  </ScannerText>
-                </div>
-                <div className="flex mt-1">
-                  <span className="mr-2">{tx?.TransactionType}</span>
-                  <span className="flex-auto"></span>
-                  {tx?.TransactionType === "Payment" && (
-                    <Typography.Text>
-                      {dropsToXrp(tx.Amount.toString())} XRP (
-                      {isRecipient ? "Received" : "Sent"})
-                    </Typography.Text>
-                  )}
-                  {tx?.TransactionType === "NFTokenMint" && (
-                    <ScannerText
-                      className="text-xs"
-                      type="ipfs"
-                      href={`/${convertHexToString(tx.URI ?? "")}`}
-                    >
-                      {convertHexToString(tx.URI ?? "-")}
-                    </ScannerText>
-                  )}
-                  {tx?.TransactionType === "TicketCreate" && (
-                    <Typography.Text>
-                      Created {tx.TicketCount} Tickets from Sequence{" "}
-                      {tx.Sequence}
-                    </Typography.Text>
-                  )}
-                </div>
-              </div>
-            );
-          })}
+                    <div className="flex mb-2">
+                      <span className="mr-2">Sequence</span>
+                      <span className="flex-auto"></span>
+                      <Typography.Text className="text-xs">
+                        {tx?.Sequence === 0
+                          ? `${tx.TicketSequence} (Ticket)`
+                          : tx?.Sequence}
+                      </Typography.Text>
+                    </div>
+                    <div className="flex">
+                      <span className="mr-2">Tx Hash</span>
+                      <span className="flex-auto"></span>
+                      <ScannerText
+                        className="text-xs"
+                        href={`/transactions/${tx?.hash}`}
+                      >
+                        {tx?.hash}
+                      </ScannerText>
+                    </div>
+                    <div className="flex mt-1">
+                      <span className="mr-2">{tx?.TransactionType}</span>
+                      <span className="flex-auto"></span>
+                      {tx?.TransactionType === "Payment" && (
+                        <Typography.Text>
+                          {dropsToXrp(tx.Amount.toString())} XRP (
+                          {isRecipient ? "Received" : "Sent"})
+                        </Typography.Text>
+                      )}
+                      {tx?.TransactionType === "NFTokenMint" && (
+                        <ScannerText
+                          className="text-xs"
+                          type="ipfs"
+                          href={`/${convertHexToString(tx.URI ?? "")}`}
+                        >
+                          {convertHexToString(tx.URI ?? "-")}
+                        </ScannerText>
+                      )}
+                      {tx?.TransactionType === "TicketCreate" && (
+                        <Typography.Text>
+                          Created {tx.TicketCount} Tickets from Sequence{" "}
+                          {tx.Sequence}
+                        </Typography.Text>
+                      )}
+                    </div>
+                  </div>
+                );
+              })}
+          </InfiniteScroll>
         </div>
       </div>
     </>
